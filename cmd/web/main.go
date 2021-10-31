17d0bc6c57abbfa2792830eae2ad3912c21bb677
Shou Tseng <yakushou730@gmail.com>
2021-10-31 21:53:10 +0800

    feat: apply database manipulation

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,12 +7,15 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/yakushou730/lets-go/pkg/models/mysql"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *mysql.SnippetModel
 }
 
 func main() {
@@ -32,6 +35,7 @@ func main() {
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &mysql.SnippetModel{DB: db},
 	}
 
 	srv := &http.Server{
