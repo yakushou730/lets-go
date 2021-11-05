6759cab6e8f924ac06e34009a8b9007a686341f8
Shou Tseng <yakushou730@gmail.com>
2021-11-05 15:17:51 +0800

    feat: adjust redirect url after user login

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -150,6 +150,11 @@ func (app *application) loginUser(w http.ResponseWriter, r *http.Request) {
 	}
 	app.session.Put(r, "authenticatedUserID", id)
 
+	path := app.session.PopString(r, "redirectPathAfterLogin")
+	if path != "" {
+		http.Redirect(w, r, path, http.StatusSeeOther)
+		return
+	}
 	http.Redirect(w, r, "/snippet/create", http.StatusSeeOther)
 }
 
